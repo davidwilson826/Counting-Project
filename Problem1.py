7c084b56dd447f4a2096e5c4bac882dbf37c08dc
davidwilson826 <davidwilson@hanovernorwichschools.org>
2017-05-09 12:33:06 -0400

    Updated from Brython Server: 5/9/2017 12:33:05 PM

--- Problem1.py
+++ Problem1.py
@@ -24,16 +24,17 @@ people = ['T', 'T', 'T', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 
 
 successes = 0
 amount = 0
-trials = 1000
+trials = 10
 
 for i in range(trials):
     shuffle(people)
-    #print(people)
+    print(people)
     for j in range(10):
         #print(people[j] + people[j+1] + people[j+2] + people[j+3])
         if (people[j] + people[j+1] + people[j+2] + people[j+3] + people[j+4]) == 'SSTSS':
             amount += 1
         if amount == 3:
             successes += 1
+        amount = 0
         
 print((successes)/trials*100, '%')
