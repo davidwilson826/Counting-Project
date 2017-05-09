9b793d0d34765962d459b91412b1c473a63aa538
davidwilson826 <davidwilson@hanovernorwichschools.org>
2017-05-09 12:27:08 -0400

    Updated from Brython Server: 5/9/2017 12:27:07 PM

--- Problem1.py
+++ Problem1.py
@@ -1,3 +1,4 @@
+'''
 from random import shuffle, randint
 
 cards = ['J', 'J', 'J', 'J', 'Q', 'Q', 'Q', 'Q', 'K', 'K', 'K', 'K']
@@ -11,6 +12,25 @@ for i in range(trials):
     for j in range(9):
         #print(cards[j] + cards[j+1] + cards[j+2] + cards[j+3])
         if (cards[j] + cards[j+1] + cards[j+2] + cards[j+3]) in ['JJJJ', 'QQQQ', 'KKKK']:
+            failures += 1
+            break
+        
+print((trials-failures)/trials*100, '%')
+'''
+
+from random import shuffle, randint
+
+people = ['T', 'T', 'T', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S']
+
+failures = 0
+trials = 1000
+
+for i in range(trials):
+    shuffle(people)
+    #print(people)
+    for j in range(9):
+        #print(people[j] + people[j+1] + people[j+2] + people[j+3])
+        if (people[j] + people[j+1] + people[j+2] + people[j+3]) in ['JJJJ', 'QQQQ', 'KKKK']:
             failures += 1
             break
         
