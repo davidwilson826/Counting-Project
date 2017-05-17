7804e43f0019e3db33552520c10c2f882630450f
davidwilson826 <davidwilson@hanovernorwichschools.org>
2017-05-17 10:25:06 -0400

    Updated from Brython Server: 5/17/2017 10:25:05 AM

--- Problem1.py
+++ Problem1.py
@@ -1,44 +1,44 @@
-
+'''
 from random import shuffle, randint
 
 cards = ['J', 'J', 'J', 'J', 'Q', 'Q', 'Q', 'Q', 'K', 'K', 'K', 'K']
 
 failures = 0
-trials = 10
+trials = 1000
 
 for i in range(trials):
     shuffle(cards)
-    print(cards)
+    #print(cards)
     for j in range(9):
         if (cards[j] + cards[j+1] + cards[j+2] + cards[j+3]) in ['JJJJ', 'QQQQ', 'KKKK']:
             failures += 1
             break
         
 print((trials-failures)/trials*100, '%')
-
 '''
+
 from random import shuffle, randint
 
 people = ['T', 'T', 'T', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S']
 
 successes = 0
 amount = 0
-trials = 1000
+trials = 10
 
 for i in range(trials):
     shuffle(people)
-    #print(people)
+    print(people)
     for j in range(11):
         #print(people[j] + people[j+1] + people[j+2] + people[j+3])
         if (people[j] + people[j+1] + people[j+2] + people[j+3] + people[j+4]) == 'SSTSS':
             amount += 1
         #print(amount)
-        if amount == 3:
-            successes += 1
+    if amount == 3:
+        successes += 1
     amount = 0
         
 print((successes)/trials*100, '%')
-'''
+
 '''
 from random import shuffle, randint
 
